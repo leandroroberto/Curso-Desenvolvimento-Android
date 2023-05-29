792cf174729cf3edb4d432f3d1dc1f73ac296016
Leandro <lecodriver@hotmail.com>
2023-05-29 00:46:52 -0300

    Uso dos métodos Getter e Setter

--- AppListaCurso/app/src/main/java/devandroid/leandro/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/leandro/applistacurso/model/Pessoa.java
@@ -3,8 +3,44 @@ package devandroid.leandro.applistacurso.model;
 //Classe Pessoa
 public class Pessoa {
 
-    //Construtor
-    public Pessoa(){
+    //Atributos
+    private String primeiroNome;
+    private String sobrenome;
+    private String cursoDesejado;
+    private String telefoneContato;
 
+    //Construtor
+    public Pessoa(){}
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
     }
 }
--- AppListaCurso/app/src/main/java/devandroid/leandro/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/leandro/applistacurso/view/MainActivity.java
@@ -9,12 +9,49 @@ import devandroid.leandro.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
 
-    Pessoa pessoa;
+    Pessoa pessoa = new Pessoa();
+    Pessoa outraPessoa = new Pessoa();
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pessoa = new Pessoa();
+
+        //Atribuições a pessoa,
+        pessoa.setPrimeiroNome("Leandro");
+        pessoa.setSobrenome("Roberto");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11-952299631");
+
+        //Atribuição a outraPessoa através do SETTER
+        outraPessoa.setPrimeiroNome("Noah");
+        outraPessoa.setSobrenome("Gabriel");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("11-965829383");
+
+
+        //Resgatando valores a partir do GETTER de 'pessoa'
+        dadosPessoa = "Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobrenome();
+        dadosPessoa += " Curso desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        //Resgatando valores a partir do GETTER de 'outraPessoa'
+        dadosOutraPessoa = "Nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobrenome();
+        dadosOutraPessoa += " Curso desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
     }
 }
